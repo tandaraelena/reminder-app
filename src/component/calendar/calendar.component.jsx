1c41a39384ad926ede5314fcb7f06006815352fe
tandaraelena <tandaraelena@yahoo.com>
2019-11-12 21:01:49 +0000

    display the days of the month

--- src/component/calendar/calendar.component.jsx
+++ src/component/calendar/calendar.component.jsx
@@ -9,12 +9,55 @@ const Calendar = () => {
   
   const renderCalendarHeader = () => {
     return weekdays.map(day => (
-      <div>{day}</div>
+      <div key={day}>{day}</div>
     ))
   }
+
+  const renderCalendarBody = () => {
+    return dayList.map(({ date }) => (
+      <div key={date}>{date}</div>
+    ))
+  }
+
+  const firstDayInMonth = moment()
+    .locale('en-gb')
+    .date(1)
+    .hour(0)
+    .minute(0)
+    .second(0);
+
+  const firstDayInCalendar = moment()
+    .locale('en-gb')
+    .date(1)
+    .hour(0)
+    .minute(0)
+    .second(0)
+    .subtract(
+      moment()
+        .locale('en-gb')
+        .date(1)
+        .hour(0)
+        .minute(0)
+        .second(0).weekday() - 1,
+        'd'
+    )
+  
+  const firstUnixTime = firstDayInCalendar.unix();
+
+  const dayList = Array
+    .from({length: 35}, (_,key) => key)
+    .map(n => ({
+      time: firstUnixTime*1000 + n*60*60*24*1000
+    }))
+    .map(({ time }) => ({
+      time,
+      date: moment(time).format('MMM Do YYYY')
+    }))
+
   return (
     <StyledCalendar>
       {renderCalendarHeader()}
+      {renderCalendarBody()}
     </StyledCalendar>
   )
 }
