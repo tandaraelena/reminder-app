e8dc0d57816d1de083a1bd3bef502f2a24dce0b8
tandaraelena <tandaraelena@yahoo.com>
2019-11-12 21:32:15 +0000

    create a reducer for adding a new reminder

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { ActionBar } from '../action-bar';
 import { Calendar } from '../calendar';
 import { Modal } from '../modal';
 
+const SET_REMINDER_DATE = 'SET_REMINDER_DATE';
+
+const initialValue = null;
+
+const createReminderAction = date => ({
+  type: SET_REMINDER_DATE,
+  date,
+})
+
+const modalReducer = (state = initialValue, action) => {
+  switch(action.type) {
+    case SET_REMINDER_DATE:
+      return {
+        date: action.date
+      }
+    default:
+      return state
+  }
+}
+
 const ReminderApp = () => {
+  const [currentReminder, updateReminder] = useReducer(modalReducer, initialValue);
+
   return (
     <div>
       <ActionBar/>
